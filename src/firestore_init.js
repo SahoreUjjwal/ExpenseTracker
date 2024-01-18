01330b3cdc5e8a4403d0750c3bd3bcaf6c2cf00c
ujjwal shaore <sahoreujjwal@gmail.com>
2024-01-18 14:30:50 +0530

    Expense tracking app first commit

---
+++ src/firestore_init.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcVV_74FD524jDSyLqJBazWozlKnzz58o",
+  authDomain: "blog-007-411518.firebaseapp.com",
+  projectId: "blog-007-411518",
+  storageBucket: "blog-007-411518.appspot.com",
+  messagingSenderId: "421789584415",
+  appId: "1:421789584415:web:ee8e0844654816aba1992a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
